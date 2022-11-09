e3783dd358e5d78e1a1d9f30873bcbd15e87b2d6
Tarique Khan <tarique.khan3071@gmail.com>
2022-11-09 17:55:39 +0530

    fixed flutter web issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
+## 1.0.2
+* Fixed Flutter Web issue
+
 ## 1.0.1
 * Added support for BoxDecoration
 
 
 ## 1.0.0
-
 * Allows manual configuration of Expansion Tile such as Expansion, Toggle, Collapsing
--- lib/advance_expansion_tile.dart
+++ lib/advance_expansion_tile.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 const Duration _kExpand = Duration(milliseconds: 200);
